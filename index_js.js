c7ec575e536a60da750ba4eb05644f1b1816f9a6
LittleDC <1187061276@qq.com>
2021-04-06 15:46:28 +0800

    .

--- index_js.js
+++ index_js.js
@@ -1,67 +1,69 @@
-<!-- index原版多余js，我也不知道要干嘛 -->
-<script>
-    // @formatter:off
-    document.addEventListener("DOMContentLoaded", function () {
-        window.ApexCharts && (new ApexCharts(document.getElementById('chart-revenue-bg'), {
-            chart: {
-                type: "area",
-                fontFamily: 'inherit',
-                height: 40.0,
-                sparkline: {
-                    enabled: true
-                },
-                animations: {
-                    enabled: false
-                },
-            },
-            dataLabels: {
-                enabled: false,
-            },
-            fill: {
-                opacity: .16,
-                type: 'solid'
-            },
-            stroke: {
-                width: 2,
-                lineCap: "round",
-                curve: "smooth",
-            },
-            series: [{
-                name: "Profits",
-                data: [37, 35, 44, 28, 36, 24, 65, 31, 37, 39, 62, 51, 35, 41, 35, 27, 93, 53, 61, 27, 54, 43, 19, 46, 39, 62, 51, 35, 41, 67]
-            }],
-            grid: {
-                strokeDashArray: 4,
-            },
-            xaxis: {
-                labels: {
-                    padding: 0
-                },
-                tooltip: {
-                    enabled: false
-                },
-                axisBorder: {
-                    show: false,
-                },
-                type: 'datetime',
-            },
-            yaxis: {
-                labels: {
-                    padding: 4
-                },
-            },
-            labels: [
-                '2020-06-20', '2020-06-21', '2020-06-22', '2020-06-23', '2020-06-24', '2020-06-25', '2020-06-26', '2020-06-27', '2020-06-28', '2020-06-29', '2020-06-30', '2020-07-01', '2020-07-02', '2020-07-03', '2020-07-04', '2020-07-05', '2020-07-06', '2020-07-07', '2020-07-08', '2020-07-09', '2020-07-10', '2020-07-11', '2020-07-12', '2020-07-13', '2020-07-14', '2020-07-15', '2020-07-16', '2020-07-17', '2020-07-18', '2020-07-19'
-            ],
-            colors: ["#206bc4"],
-            legend: {
-                show: false,
-            },
-        })).render();
-    });
-      // @formatter:on
-</script>
-<script>
+// @formatter:off
+document.addEventListener("DOMContentLoaded", function () {
+window.ApexCharts && (new ApexCharts(document.getElementById('chart-revenue-bg'), {
+chart: {
+type: "area",
+fontFamily: 'inherit',
+height: 40.0,
+sparkline: {
+enabled: true
+},
+animations: {
+enabled: false
+},
+},
+dataLabels: {
+enabled: false,
+},
+fill: {
+opacity: .16,
+type: 'solid'
+},
+stroke: {
+width: 2,
+lineCap: "round",
+curve: "smooth",
+},
+series: [{
+name: "Profits",
+data: [37, 35, 44, 28, 36, 24, 65, 31, 37, 39, 62, 51, 35, 41, 35, 27, 93, 53, 61, 27, 54, 43, 19, 46, 39, 62, 51, 35,
+41, 67]
+}],
+grid: {
+strokeDashArray: 4,
+},
+xaxis: {
+labels: {
+padding: 0
+},
+tooltip: {
+enabled: false
+},
+axisBorder: {
+show: false,
+},
+type: 'datetime',
+},
+yaxis: {
+labels: {
+padding: 4
+},
+},
+labels: [
+'2020-06-20', '2020-06-21', '2020-06-22', '2020-06-23', '2020-06-24', '2020-06-25', '2020-06-26', '2020-06-27',
+'2020-06-28', '2020-06-29', '2020-06-30', '2020-07-01', '2020-07-02', '2020-07-03', '2020-07-04', '2020-07-05',
+'2020-07-06', '2020-07-07', '2020-07-08', '2020-07-09', '2020-07-10', '2020-07-11', '2020-07-12', '2020-07-13',
+'2020-07-14', '2020-07-15', '2020-07-16', '2020-07-17', '2020-07-18', '2020-07-19'
+],
+colors: ["#206bc4"],
+legend: {
+show: false,
+},
+})).render();
+});
+// @formatter:on
+  
+ 
     // @formatter:off
     document.addEventListener("DOMContentLoaded", function () {
         window.ApexCharts && (new ApexCharts(document.getElementById('chart-new-clients'), {
@@ -119,8 +121,8 @@
         })).render();
     });
       // @formatter:on
-</script>
-<script>
+  
+ 
     // @formatter:off
     document.addEventListener("DOMContentLoaded", function () {
         window.ApexCharts && (new ApexCharts(document.getElementById('chart-active-users'), {
@@ -180,8 +182,8 @@
         })).render();
     });
       // @formatter:on
-</script>
-<script>
+  
+ 
     // @formatter:off
     document.addEventListener("DOMContentLoaded", function () {
         window.ApexCharts && (new ApexCharts(document.getElementById('chart-mentions'), {
@@ -271,8 +273,8 @@
         })).render();
     });
       // @formatter:on
-</script>
-<script>
+  
+ 
     // @formatter:off
     document.addEventListener("DOMContentLoaded", function () {
         window.ApexCharts && (new ApexCharts(document.getElementById('sparkline-activity'), {
@@ -310,8 +312,8 @@
         })).render();
     });
       // @formatter:on
-</script>
-<script>
+  
+ 
     // @formatter:off
     document.addEventListener("DOMContentLoaded", function () {
         window.ApexCharts && (new ApexCharts(document.getElementById('chart-development-activity'), {
@@ -375,8 +377,8 @@
         })).render();
     });
       // @formatter:on
-</script>
-<script>
+  
+ 
     // @formatter:off
     document.addEventListener("DOMContentLoaded", function () {
         window.ApexCharts && (new ApexCharts(document.getElementById('sparkline-bounce-rate-1'), {
@@ -405,8 +407,8 @@
         })).render();
     });
       // @formatter:on
-</script>
-<script>
+  
+ 
     // @formatter:off
     document.addEventListener("DOMContentLoaded", function () {
         window.ApexCharts && (new ApexCharts(document.getElementById('sparkline-bounce-rate-2'), {
@@ -435,8 +437,8 @@
         })).render();
     });
       // @formatter:on
-</script>
-<script>
+  
+ 
     // @formatter:off
     document.addEventListener("DOMContentLoaded", function () {
         window.ApexCharts && (new ApexCharts(document.getElementById('sparkline-bounce-rate-3'), {
@@ -465,8 +467,8 @@
         })).render();
     });
       // @formatter:on
-</script>
-<script>
+  
+ 
     // @formatter:off
     document.addEventListener("DOMContentLoaded", function () {
         window.ApexCharts && (new ApexCharts(document.getElementById('sparkline-bounce-rate-4'), {
@@ -495,8 +497,8 @@
         })).render();
     });
       // @formatter:on
-</script>
-<script>
+  
+ 
     // @formatter:off
     document.addEventListener("DOMContentLoaded", function () {
         window.ApexCharts && (new ApexCharts(document.getElementById('sparkline-bounce-rate-5'), {
@@ -525,8 +527,8 @@
         })).render();
     });
       // @formatter:on
-</script>
-<script>
+  
+ 
     // @formatter:off
     document.addEventListener("DOMContentLoaded", function () {
         window.ApexCharts && (new ApexCharts(document.getElementById('sparkline-bounce-rate-6'), {
@@ -555,4 +557,3 @@
         })).render();
     });
       // @formatter:on
-</script>
