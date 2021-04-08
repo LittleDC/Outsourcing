92e6437bdbcc9d55cbe7d55aa2af3566136f498e
LittleDC <1187061276@qq.com>
2021-04-08 16:04:13 +0800

    profile see done

---
+++ mock/profile.js
@@ -0,0 +1,18 @@
+Mock.mock('/profiles/19858190000', 'get', {
+    "data": {
+        "staff_name": "DC",
+        "staff_identity_card": "XXXXX",
+        "staff_sexuality": "男",
+        "staff_marry": "母胎单身",
+        "staff_phone": "19800000000",
+        "staff_school": "HDU",
+        "staff_major": "计算机专业",
+        "staff_education": "大学",
+        "staff_photo": "",
+        "staff_sertification": "",
+        "staff_character": "",
+        "staff_experience": ""
+    },
+    "msg": "",
+    "status": ""
+})
